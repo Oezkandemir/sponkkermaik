64e07f1db64be049df7328f82943a3cdb5fd9431
Oezkandemir <oezkan@reign-labs.com>
2025-11-17 11:18:42 +0100

    Refactor middleware configuration to clarify pathname matching rules, ensuring exclusion of paths starting with `/api`, `/_next`, `/_vercel`, and those containing a dot.

--- middleware.ts
+++ middleware.ts
@@ -29,6 +29,11 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(de|en)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    '/((?!api|_next|_vercel|.*\\..*).*)'
+  ]
 };
 
